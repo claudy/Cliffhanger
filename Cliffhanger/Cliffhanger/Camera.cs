df0b9feb286c228aecd1bc0413912671efdb9de5
Clayton Sandham <sandhamcr1@gmail.com>
2013-09-24 15:33:04 -0400

    added the camera.cs file.  This class contains a 3d camera from our going postal class. The member variables and functions should be easily modifyable.  I recall it being good code to start from, but if it's better scrapped, so be it.

--- Cliffhanger/Cliffhanger/Camera.cs
+++ Cliffhanger/Cliffhanger/Camera.cs
@@ -17,32 +17,154 @@ namespace Cliffhanger
     /// </summary>
     public class Camera : Microsoft.Xna.Framework.GameComponent
     {
-        public Camera(Game game)
+        public Matrix view { get; set; }
+        public Matrix projection { get; set; }
+        public Vector3 cameraPos, cameraDisplacement;
+        public float radius = .1f;
+
+        private KeyboardState ks;
+        private MouseState previousMouseState;
+        public Vector3 lookAt;
+        private Vector3 cameraTarget = Vector3.Zero;
+        private Vector3 cameraUpVector = Vector3.Up;
+        private Vector3 cameraReference = new Vector3(0.0f, 0.0f, -1.0f);
+
+        private float yaw;
+        private float pitch;
+        private float roll;
+
+        private float movementVelocity = .5f;
+
+        private float spinRate = 90f;
+
+        private Vector3 dir; //direction the user wants to move the camera
+
+        private float timeDelta; //time between calls to  update
+
+        public Camera(Game game, Vector3 pos, Vector3 target, Vector3 up)
             : base(game)
         {
-            // TODO: Construct any child components here
+            // Initialize view matrix
+            cameraPos = pos;
+            lookAt = target;
+            view = Matrix.CreateLookAt(pos, target, up);
+            cameraDisplacement = Vector3.Zero;
+            // Initialize projection matrix
+            projection = Matrix.CreatePerspectiveFieldOfView(
+                MathHelper.Pi / 8f,
+                (float)Game.Window.ClientBounds.Width /
+                (float)Game.Window.ClientBounds.Height,
+                radius, 5000);
         }
 
-        /// <summary>
-        /// Allows the game component to perform any initialization it needs to before starting
-        /// to run.  This is where it can query for any required services and load content.
-        /// </summary>
+
         public override void Initialize()
         {
-            // TODO: Add your initialization code here
 
             base.Initialize();
         }
 
-        /// <summary>
-        /// Allows the game component to update itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        private void cameraInput()
+        {
+
+            dir = new Vector3(0);
+            ks = Keyboard.GetState();
+            MouseState mState = Mouse.GetState();
+
+            if (ks.IsKeyDown(Keys.I)) dir.Z--;
+            if (ks.IsKeyDown(Keys.K)) dir.Z++;
+            if (ks.IsKeyDown(Keys.J)) dir.X--;
+            if (ks.IsKeyDown(Keys.L)) dir.X++;
+
+            if (ks.IsKeyDown(Keys.OemPeriod)) dir.Y++;
+            if (ks.IsKeyDown(Keys.OemComma)) dir.Y--;
+
+            if (ks.IsKeyDown(Keys.R)) cameraDisplacement = Vector3.Zero;
+
+            if ((previousMouseState.X > mState.X) && (mState.LeftButton == ButtonState.Pressed))
+            {
+                yaw += (spinRate * timeDelta);
+            }
+            else if ((previousMouseState.X < mState.X) && (mState.LeftButton == ButtonState.Pressed))
+            {
+                yaw -= (spinRate * timeDelta);
+            }
+            if (yaw > 360)
+                yaw -= 360;
+            else if (yaw < 0)
+                yaw += 360;
+
+            if ((previousMouseState.Y > mState.Y) && (mState.LeftButton == ButtonState.Pressed))
+                pitch += (spinRate * timeDelta);
+            else if ((previousMouseState.Y < mState.Y) && (mState.LeftButton == ButtonState.Pressed))
+                pitch -= (spinRate * timeDelta);
+            if (pitch > 360)
+                pitch -= 360;
+            else if (pitch < 0)
+                pitch += 360;
+            if ((previousMouseState.ScrollWheelValue > mState.ScrollWheelValue))
+                roll++;
+            else if ((previousMouseState.ScrollWheelValue < mState.ScrollWheelValue))
+                roll--;
+            if (roll > 360)
+                roll -= 360;
+            else if (roll < 0)
+                roll += 360;
+            previousMouseState = mState;
+        }
         public override void Update(GameTime gameTime)
         {
-            // TODO: Add your update code here
+            timeDelta = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            //cameraInput();
+
+
+
+            Matrix yawR = Matrix.CreateRotationY(MathHelper.ToRadians(yaw));
+            Matrix pitchR = Matrix.CreateRotationX(MathHelper.ToRadians(pitch));
+            Matrix rollr = Matrix.CreateRotationX(MathHelper.ToRadians(roll));
+
+            Matrix dirRotation = Matrix.Identity;
+
+            Vector3 dt = Vector3.Zero;
+
+            // cameraPos update
+            if (dir != Vector3.Zero)
+            {
+
+                dir.Normalize();
+                dt = movementVelocity * dir * gameTime.ElapsedGameTime.Milliseconds;
+                Vector3.Transform(ref dt, ref dirRotation, out dt);
+                cameraDisplacement += dt;
+            }
+            //lookAt Update
+            int cameraMovementCap = 300;
+            if (cameraDisplacement.X > cameraMovementCap)
+                cameraDisplacement.X = cameraMovementCap;
+            if (cameraDisplacement.X < -cameraMovementCap)
+                cameraDisplacement.X = -cameraMovementCap;
+
+            if (cameraDisplacement.Y > cameraMovementCap)
+                cameraDisplacement.Y = cameraMovementCap;
+            if (cameraDisplacement.Y < -cameraMovementCap)
+                cameraDisplacement.Y = -cameraMovementCap;
+
+            if (cameraDisplacement.Z > cameraMovementCap)
+                cameraDisplacement.Z = cameraMovementCap;
+            if (cameraDisplacement.Z < -cameraMovementCap)
+                cameraDisplacement.Z = -cameraMovementCap;
+
+            Matrix bar = view;
+            cameraTarget = lookAt + dt;
+            lookAt += dt;
+            Vector3 foo = cameraPos + cameraDisplacement;
+            Matrix.CreateLookAt(ref foo, ref cameraTarget, ref cameraUpVector, out bar);
+            view = bar;
+            roll = 0;
+
 
             base.Update(gameTime);
         }
+
+
     }
 }
