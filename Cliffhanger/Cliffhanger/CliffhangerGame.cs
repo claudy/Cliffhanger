7ce49910147dde453d56971fd200d2af0f550e0c
Andrew Claudy <andrew.claudy@live.com>
2013-09-27 18:26:56 -0400

    Adds the menu switch statement. Adds in a dummy helpScreenTexture.png.

--- Cliffhanger/Cliffhanger/CliffhangerGame.cs
+++ Cliffhanger/Cliffhanger/CliffhangerGame.cs
@@ -17,9 +17,43 @@ namespace Cliffhanger
     /// </summary>
     public class CliffhangerGame : Microsoft.Xna.Framework.Game
     {
+        enum LevelStateFSM
+        {
+            /// <summary>
+            /// Similar to the alpha dog being the leader of the pack, the alpha menu leads the rest of the levels.
+            /// </summary>
+            AlphaMenu,
+            Level1,
+            Level1Ending,
+            Level2,
+            Level2Ending,
+            Level3,
+            Level3Ending
+        }
+
+        /// <summary>
+        /// Finite State Machine of the game mode/current level.
+        /// </summary>
+        LevelStateFSM currentGameState;
+        enum MenuState
+        {
+            TopMost,
+            Help,
+            Exit
+        }
+
+        /// <summary>
+        /// Finite State Machine of the menu.
+        /// </summary>
+        MenuState currentMenuState;
+        
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         ClaudyInput input;
+
+        #region Textures
+        Texture2D helpScreenTexture;
+        #endregion
 
         public CliffhangerGame()
         {
@@ -30,7 +64,8 @@ namespace Cliffhanger
 
         protected override void Initialize()
         {
-            
+            currentGameState = LevelStateFSM.AlphaMenu;
+            currentMenuState = MenuState.TopMost;
             base.Initialize();
         }
 
@@ -39,6 +74,7 @@ namespace Cliffhanger
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
+            helpScreenTexture = Content.Load<Texture2D>("helpScreenTexture");
         }
 
         protected override void UnloadContent()
@@ -53,11 +89,44 @@ namespace Cliffhanger
         protected override void Update(GameTime gameTime)
         {
             input.Update(); // ONLY MAKE THIS CALL ONCE PER FRAME!!!1
-
             // Allows the game to exit
             if (input.DetectBackPressedByAnyPlayer())
                 this.Exit();
+            //////////////////////////////////////
 
+            switch (currentGameState)
+            {
+                case LevelStateFSM.AlphaMenu:
+                    #region Main Menu
+                    switch (currentMenuState)
+	                {
+                        case MenuState.TopMost:
+                            break;
+                        case MenuState.Help:
+                            break;
+                        case MenuState.Exit:
+                            this.Exit();
+                            break;
+                        default:
+                            break;
+	                }
+                    #endregion
+                    break;
+                case LevelStateFSM.Level1:
+                    break;
+                case LevelStateFSM.Level1Ending:
+                    break;
+                case LevelStateFSM.Level2:
+                    break;
+                case LevelStateFSM.Level2Ending:
+                    break;
+                case LevelStateFSM.Level3:
+                    break;
+                case LevelStateFSM.Level3Ending:
+                    break;
+                default:
+                    break;
+            }
 
             base.Update(gameTime);
         }
@@ -69,11 +138,45 @@ namespace Cliffhanger
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+            spriteBatch.Begin();
 
-            // TODO: Add your drawing code here
-
+            switch (currentGameState)
+            {
+                case LevelStateFSM.AlphaMenu:
+                    #region Main Menu
+                    switch (currentMenuState)
+                    {
+                        case MenuState.TopMost:
+                            break;
+                        case MenuState.Help:
+                            //TODO: Draw 1920x1080 texture which explains how to play the game.
+                            spriteBatch.Draw(helpScreenTexture, GraphicsDevice.Viewport.Bounds, Color.White);
+                            break;
+                        case MenuState.Exit:
+                            break;
+                        default:
+                            break;
+                    }
+                    #endregion
+                    break;
+                case LevelStateFSM.Level1:
+                    break;
+                case LevelStateFSM.Level1Ending:
+                    break;
+                case LevelStateFSM.Level2:
+                    break;
+                case LevelStateFSM.Level2Ending:
+                    break;
+                case LevelStateFSM.Level3:
+                    break;
+                case LevelStateFSM.Level3Ending:
+                    break;
+                default:
+                    break;
+            }
             // Claudy calls dibs on viewports drawing code.
 
+            spriteBatch.End();
             base.Draw(gameTime);
         }
     }
