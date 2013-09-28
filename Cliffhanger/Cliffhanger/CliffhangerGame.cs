3c1596cf8260750d82cd3e32170716d78e5786c8
Andrew Claudy <andrew.claudy@live.com>
2013-09-27 22:58:21 -0400

    Game Menu works perfectly.

--- Cliffhanger/Cliffhanger/CliffhangerGame.cs
+++ Cliffhanger/Cliffhanger/CliffhangerGame.cs
@@ -35,31 +35,11 @@ namespace Cliffhanger
         /// Finite State Machine of the game mode/current level.
         /// </summary>
         public LevelStateFSM currentGameState;
-        enum MenuState 
-        {
-            TopMost,
-            Help,
-            InGame,
-            Exit
-        }
-
-        /// <summary>
-        /// Finite State Machine of the menu.
-        /// </summary>
-        MenuState currentMenuState;
-
-        enum MenuChoice // THIS ENUMERATION MUST BE IN ORDER.
-        {
-            Play, 
-            Help,
-            Exit // THIS MUST BE LAST.
-        }
-
-        MenuChoice currentlySelectedMenuChoice;
+        Menu mainMenu;
 
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-        ClaudyInput input;
+        public ClaudyInput input;
 
         //Viewport stuff
         RenderTarget2D topScreen;
@@ -75,20 +55,18 @@ namespace Cliffhanger
         Rectangle cliffRect;
         int cliffTop, cliffBottom;
 
-        #region Textures
+        //Textures
         Texture2D helpScreenTexture;
-        #endregion
+        Texture2D menuScreenTexture;
 
-        SpriteFont calibri, consolas;
-        Vector2 playMenuItemPos, helpMenuItempPos, exitMenuItemPos;
-        readonly Color colorSelectYES = Color.Red, colorSelectNO = Color.White;
+        public SpriteFont calibri, consolas;
 
         public CliffhangerGame()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             input = ClaudyInput.Instance;
-            graphics.IsFullScreen = true;
+            graphics.IsFullScreen = false; //TODO: MAKE TRUE ON SATURDAY AFTERNOON
         }
 
         protected override void Initialize()
@@ -99,8 +77,9 @@ namespace Cliffhanger
             cliffTop = 0;
             cliffBottom = 0;
             currentGameState = LevelStateFSM.AlphaMenu;
-            currentMenuState = MenuState.TopMost;
-            currentlySelectedMenuChoice = MenuChoice.Help;            base.Initialize();
+
+
+            base.Initialize();
         }
 
         protected override void LoadContent()
@@ -113,10 +92,8 @@ namespace Cliffhanger
             consolas = Content.Load<SpriteFont>("consolas");
 
             helpScreenTexture = Content.Load<Texture2D>("helpScreenTexture");
-
-            playMenuItemPos = new Vector2(GraphicsDevice.Viewport.Width / 4.0f, GraphicsDevice.Viewport.Height / 2.0f - GraphicsDevice.Viewport.Height * 0.1f);
-            helpMenuItempPos = new Vector2(GraphicsDevice.Viewport.Width / 4.0f, GraphicsDevice.Viewport.Height / 2.0f * 1.0f);
-            exitMenuItemPos =  new Vector2(GraphicsDevice.Viewport.Width / 4.0f, GraphicsDevice.Viewport.Height / 2.0f + GraphicsDevice.Viewport.Height * 0.1f);
+            menuScreenTexture = Content.Load<Texture2D>("menuScreenTexture");
+            mainMenu = new Menu(this);
         }
 
         protected override void UnloadContent()
@@ -139,74 +116,7 @@ namespace Cliffhanger
             switch (currentGameState)
             {
                 case LevelStateFSM.AlphaMenu:
-                    #region Main Menu
-                    switch (currentMenuState)
-	                {
-                        case MenuState.TopMost:
-                            for (int pi = 1; pi <= 2; pi++)
-                            {
-                                if (input.GamepadByID[pi].IsConnected)
-                                {
-                                    if ((input.GamepadByID[pi].DPad.Up == ButtonState.Pressed && input.PreviousGamepadByID[pi].DPad.Up == ButtonState.Released) ||
-                                        (input.GamepadByID[pi].ThumbSticks.Left.Y > 0.5f && input.PreviousGamepadByID[pi].ThumbSticks.Left.Y <= 0.5f) ||
-                                        (input.GamepadByID[pi].ThumbSticks.Right.Y > 0.5f && input.PreviousGamepadByID[pi].ThumbSticks.Right.Y <= 0.5f))
-                                    {
-                                        if (currentlySelectedMenuChoice != MenuChoice.Play)
-                                            currentlySelectedMenuChoice--;
-                                    }
-                                }
-                                if (input.GamepadByID[pi].IsConnected)
-                                {
-                                    if (input.GamepadByID[pi].DPad.Down == ButtonState.Pressed && input.PreviousGamepadByID[pi].DPad.Down == ButtonState.Released||
-                                        (input.GamepadByID[pi].ThumbSticks.Left.Y < -0.5f && input.PreviousGamepadByID[pi].ThumbSticks.Left.Y >= -0.5f) ||
-                                        (input.GamepadByID[pi].ThumbSticks.Right.Y < -0.5f && input.PreviousGamepadByID[pi].ThumbSticks.Right.Y >= -0.5f))
-                                    {
-                                        if (currentlySelectedMenuChoice != MenuChoice.Exit)
-                                        currentlySelectedMenuChoice++;
-                                    }
-                                }
-                                if ((input.isFirstPress(Buttons.A, PlayerIndex.One) || input.isFirstPress(Buttons.A, PlayerIndex.Two)))
-                                {
-                                    switch (currentlySelectedMenuChoice)
-                                    {
-                                        case MenuChoice.Play:
-                                            currentMenuState = MenuState.InGame;
-                                            currentGameState = LevelStateFSM.Level1;
-                                            break;
-                                        case MenuChoice.Help:
-                                            currentMenuState = MenuState.Help;
-                                            break;
-                                        case MenuChoice.Exit:
-                                            currentMenuState = MenuState.Exit;
-                                            break;
-                                        default:
-                                            break;
-                                    }
-                                    
-                                }
-                            }
-                            break;
-                        case MenuState.Help:
-                            for (int pi = 1; pi <= 4; pi++)
-                            {
-                                if (input.GamepadByID[pi].IsConnected)
-                                {
-                                    if (input.isFirstPress(Buttons.B))
-                                    {
-                                        currentGameState = LevelStateFSM.AlphaMenu;
-                                        currentMenuState = MenuState.TopMost;
-                                        currentlySelectedMenuChoice = MenuChoice.Play;
-                                    }
-                                }
-                            }
-                            break;
-                        case MenuState.Exit:
-                            this.Exit();
-                            break;
-                        default:
-                            break;
-	                }
-                    #endregion
+                    mainMenu.Update(gameTime, this);
                     break;
                 case LevelStateFSM.Level1:
                     break;
@@ -243,51 +153,38 @@ namespace Cliffhanger
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            
+            //Draw stuff in the top renderTarget
+            graphics.GraphicsDevice.SetRenderTarget(topScreen);
+            GraphicsDevice.Clear(Color.Gray);
             spriteBatch.Begin();
+            spriteBatch.Draw(cliffTex, new Rectangle(cliffRect.X, cliffRect.Y + cliffTop, cliffRect.Width, cliffRect.Height), Color.White);
+            spriteBatch.Draw(blankTex, test, Color.Red);
+            spriteBatch.End();
+            //Draw stuff in the bottom renderTarget; Use an offset
+            graphics.GraphicsDevice.SetRenderTarget(bottomScreen);
+            GraphicsDevice.Clear(Color.Gray);
+            spriteBatch.Begin();
+            bottomOffset = GraphicsDevice.Viewport.Height;
+            spriteBatch.Draw(cliffTex, new Rectangle(cliffRect.X, cliffRect.Y - bottomOffset + cliffBottom, cliffRect.Width, cliffRect.Height), Color.White);
+            spriteBatch.Draw(blankTex, new Rectangle(test.X, test.Y - bottomOffset, test.Width, test.Height), Color.Blue);
 
+            spriteBatch.End();
+
+            //Draw the renderTargets
+            graphics.GraphicsDevice.SetRenderTarget(null);
+            spriteBatch.Begin();
+            spriteBatch.Draw(topScreen, new Vector2(0, 0), Color.White);
+            spriteBatch.Draw(bottomScreen, new Vector2(0, GraphicsDevice.Viewport.Height / 2), Color.White);
+            spriteBatch.End();
+
+            spriteBatch.Begin();
             switch (currentGameState)
             {
                 case LevelStateFSM.AlphaMenu:
-                    #region Main Menu
-                    switch (currentMenuState)
-                    {
-                        case MenuState.TopMost:
-                            //TODO: BACKGROUND TEXTURE spriteBatch.Draw(menuScreenTexture, GraphicsDevice.Viewport.Bounds, Color.White);
-                            switch (currentlySelectedMenuChoice)
-	                        {
-		                    case MenuChoice.Play:
-                                spriteBatch.DrawString(consolas, "Play", playMenuItemPos, colorSelectYES);
-                                spriteBatch.DrawString(consolas, "Help", helpMenuItempPos, colorSelectNO);
-                                spriteBatch.DrawString(consolas, "Exit", exitMenuItemPos, colorSelectNO);
-                                break;
-                            case MenuChoice.Help:
-                                spriteBatch.DrawString(consolas, "Play", playMenuItemPos, colorSelectNO);
-                                spriteBatch.DrawString(consolas, "Help", helpMenuItempPos, colorSelectYES);
-                                spriteBatch.DrawString(consolas, "Exit", exitMenuItemPos, colorSelectNO);
-                                break;
-                            case MenuChoice.Exit:
-                                spriteBatch.DrawString(consolas, "Play", playMenuItemPos, colorSelectNO);
-                                spriteBatch.DrawString(consolas, "Help", helpMenuItempPos, colorSelectNO);
-                                spriteBatch.DrawString(consolas, "Exit", exitMenuItemPos, colorSelectYES);
-                                break;
-                            default:
-                                spriteBatch.DrawString(consolas, "Play", playMenuItemPos, colorSelectNO);
-                                spriteBatch.DrawString(consolas, "Help", helpMenuItempPos, colorSelectNO);
-                                spriteBatch.DrawString(consolas, "Exit", exitMenuItemPos, colorSelectNO);
-                                break;
-                            }
-                            break;
-                        case MenuState.Help:
-                            //TODO: Draw 1920x1080 texture which explains how to play the game.
-                            spriteBatch.Draw(helpScreenTexture, GraphicsDevice.Viewport.Bounds, Color.White);
-                            break;
-                        case MenuState.Exit:
-                            break;
-                        default:
-                            break;
-                    }
-                    #endregion
+                    mainMenu.Draw(spriteBatch,
+                        consolas,
+                        menuScreenTexture,
+                        helpScreenTexture);
                     break;
                 case LevelStateFSM.Level1:
                     break;
@@ -305,32 +202,6 @@ namespace Cliffhanger
                     break;
             }
 			spriteBatch.End();
-
-
-            //Draw stuff in the top renderTarget
-            graphics.GraphicsDevice.SetRenderTarget(topScreen);
-            GraphicsDevice.Clear(Color.Gray);
-            spriteBatch.Begin();
-            spriteBatch.Draw(cliffTex, new Rectangle(cliffRect.X,cliffRect.Y + cliffTop, cliffRect.Width,cliffRect.Height), Color.White);
-            spriteBatch.Draw(blankTex, test, Color.Red);
-            spriteBatch.End();
-            //Draw stuff in the bottom renderTarget; Use an offset
-            graphics.GraphicsDevice.SetRenderTarget(bottomScreen);
-            GraphicsDevice.Clear(Color.Gray);
-            spriteBatch.Begin();
-            bottomOffset = GraphicsDevice.Viewport.Height;
-            spriteBatch.Draw(cliffTex, new Rectangle(cliffRect.X,cliffRect.Y - bottomOffset + cliffBottom, cliffRect.Width, cliffRect.Height), Color.White);
-            spriteBatch.Draw(blankTex, new Rectangle(test.X,test.Y - bottomOffset,test.Width,test.Height), Color.Blue);
-            
-            spriteBatch.End();
-
-            //Draw the renderTargets
-            graphics.GraphicsDevice.SetRenderTarget(null);
-            spriteBatch.Begin();
-            spriteBatch.Draw(topScreen, new Vector2(0, 0), Color.White);
-            spriteBatch.Draw(bottomScreen, new Vector2(0, GraphicsDevice.Viewport.Height / 2), Color.White);
-            spriteBatch.End();
-
             base.Draw(gameTime);
         }
     }
--- Cliffhanger/Cliffhanger/Menu.cs
+++ Cliffhanger/Cliffhanger/Menu.cs
@@ -9,6 +9,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 
+using Claudy.Input;
 
 namespace Cliffhanger
 {
@@ -17,10 +18,47 @@ namespace Cliffhanger
     /// </summary>
     public class Menu : Microsoft.Xna.Framework.GameComponent
     {
-        public Menu(Game game)
+        public readonly Color colorSelectYES = Color.Red, colorSelectNO = Color.White;
+        public Vector2 playMenuItemPos, helpMenuItempPos, exitMenuItemPos;
+        private ClaudyInput input;
+
+        enum MenuState
+        {
+            TopMost,
+            Help,
+            InGame,
+            Exit
+        }
+
+        /// <summary>
+        /// Finite State Machine of the menu.
+        /// </summary>
+        MenuState currentMenuState;
+
+        enum MenuChoice // THIS ENUMERATION MUST BE IN ORDER.
+        {
+            Play,
+            Help,
+            Exit // THIS MUST BE LAST.
+        }
+
+        MenuChoice currentlySelectedMenuChoice;
+
+
+        public Menu(CliffhangerGame game)
             : base(game)
         {
-            // TODO: Construct any child components here
+            currentMenuState = MenuState.TopMost;
+            currentlySelectedMenuChoice = MenuChoice.Help;
+
+            playMenuItemPos = new Vector2(Game.GraphicsDevice.Viewport.Width / 4.0f,
+                Game.GraphicsDevice.Viewport.Height / 2.0f - Game.GraphicsDevice.Viewport.Height * 0.1f);
+            helpMenuItempPos = new Vector2(Game.GraphicsDevice.Viewport.Width / 4.0f,
+                Game.GraphicsDevice.Viewport.Height / 2.0f * 1.0f);
+            exitMenuItemPos = new Vector2(Game.GraphicsDevice.Viewport.Width / 4.0f, 
+                Game.GraphicsDevice.Viewport.Height / 2.0f + Game.GraphicsDevice.Viewport.Height * 0.1f);
+
+            input = game.input;
         }
 
         /// <summary>
@@ -29,8 +67,6 @@ namespace Cliffhanger
         /// </summary>
         public override void Initialize()
         {
-            // TODO: Add your initialization code here
-
             base.Initialize();
         }
 
@@ -38,15 +74,126 @@ namespace Cliffhanger
         /// Allows the game component to update itself.
         /// </summary>
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
-        public override void Update(GameTime gameTime)
+        public void Update(GameTime gameTime, CliffhangerGame game)
         {
-            // TODO: Add your update code here
+            #region Main Menu
+            switch (currentMenuState)
+            {
+                case MenuState.TopMost:
+                    for (int pi = 1; pi <= 2; pi++)
+                    {
+                        if (input.GamepadByID[pi].IsConnected)
+                        {
+                            if ((input.GamepadByID[pi].DPad.Up == ButtonState.Pressed && input.PreviousGamepadByID[pi].DPad.Up == ButtonState.Released) ||
+                                (input.GamepadByID[pi].ThumbSticks.Left.Y > 0.5f && input.PreviousGamepadByID[pi].ThumbSticks.Left.Y <= 0.5f) ||
+                                (input.GamepadByID[pi].ThumbSticks.Right.Y > 0.5f && input.PreviousGamepadByID[pi].ThumbSticks.Right.Y <= 0.5f))
+                            {
+                                if (currentlySelectedMenuChoice != MenuChoice.Play)
+                                    currentlySelectedMenuChoice--;
+                            }
+                        }
+                        if (input.GamepadByID[pi].IsConnected)
+                        {
+                            if (input.GamepadByID[pi].DPad.Down == ButtonState.Pressed && input.PreviousGamepadByID[pi].DPad.Down == ButtonState.Released ||
+                                (input.GamepadByID[pi].ThumbSticks.Left.Y < -0.5f && input.PreviousGamepadByID[pi].ThumbSticks.Left.Y >= -0.5f) ||
+                                (input.GamepadByID[pi].ThumbSticks.Right.Y < -0.5f && input.PreviousGamepadByID[pi].ThumbSticks.Right.Y >= -0.5f))
+                            {
+                                if (currentlySelectedMenuChoice != MenuChoice.Exit)
+                                    currentlySelectedMenuChoice++;
+                            }
+                        }
+                        if ((input.isFirstPress(Buttons.A, PlayerIndex.One) || input.isFirstPress(Buttons.A, PlayerIndex.Two)))
+                        {
+                            switch (currentlySelectedMenuChoice)
+                            {
+                                case MenuChoice.Play:
+                                    currentMenuState = MenuState.InGame;
+                                    game.currentGameState = CliffhangerGame.LevelStateFSM.Level1;
+                                    break;
+                                case MenuChoice.Help:
+                                    currentMenuState = MenuState.Help;
+                                    break;
+                                case MenuChoice.Exit:
+                                    currentMenuState = MenuState.Exit;
+                                    break;
+                                default:
+                                    break;
+                            }
 
+                        }
+                    }
+                    break;
+                case MenuState.Help:
+                    for (int pi = 1; pi <= 4; pi++)
+                    {
+                        if (input.GamepadByID[pi].IsConnected)
+                        {
+                            if (input.isFirstPress(Buttons.B))
+                            {
+                                game.currentGameState = CliffhangerGame.LevelStateFSM.AlphaMenu;
+                                currentMenuState = MenuState.TopMost;
+                                currentlySelectedMenuChoice = MenuChoice.Play;
+                            }
+                        }
+                    }
+                    break;
+                case MenuState.Exit:
+                    game.Exit();
+                    break;
+                default:
+                    break;
+            }
+            #endregion
             base.Update(gameTime);
         }
 
-        public void Draw(SpriteBatch spriteBatch)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="spriteBatch">The spriteBatch passed MUST have its .Begin() call first.</param>
+        public void Draw(SpriteBatch spriteBatch, 
+            SpriteFont menuFont, 
+            Texture2D backgroundTex, 
+            Texture2D helpBackgroundTex)
         {
+            //Assumes SpriteBatch has begun already.
+            switch (currentMenuState)
+            {
+                case MenuState.TopMost:
+                    spriteBatch.Draw(backgroundTex, Game.GraphicsDevice.Viewport.Bounds, Color.White);
+                    switch (currentlySelectedMenuChoice)
+                    {
+                        case MenuChoice.Play:
+                            spriteBatch.DrawString(menuFont, "Play", playMenuItemPos, colorSelectYES);
+                            spriteBatch.DrawString(menuFont, "Help", helpMenuItempPos, colorSelectNO);
+                            spriteBatch.DrawString(menuFont, "Exit", exitMenuItemPos, colorSelectNO);
+                            break;
+                        case MenuChoice.Help:
+                            spriteBatch.DrawString(menuFont, "Play", playMenuItemPos, colorSelectNO);
+                            spriteBatch.DrawString(menuFont, "Help", helpMenuItempPos, colorSelectYES);
+                            spriteBatch.DrawString(menuFont, "Exit", exitMenuItemPos, colorSelectNO);
+                            break;
+                        case MenuChoice.Exit:
+                            spriteBatch.DrawString(menuFont, "Play", playMenuItemPos, colorSelectNO);
+                            spriteBatch.DrawString(menuFont, "Help", helpMenuItempPos, colorSelectNO);
+                            spriteBatch.DrawString(menuFont, "Exit", exitMenuItemPos, colorSelectYES);
+                            break;
+                        default:
+                            spriteBatch.DrawString(menuFont, "Play", playMenuItemPos, colorSelectNO);
+                            spriteBatch.DrawString(menuFont, "Help", helpMenuItempPos, colorSelectNO);
+                            spriteBatch.DrawString(menuFont, "Exit", exitMenuItemPos, colorSelectNO);
+                            break;
+                    }
+                    break;
+                case MenuState.Help:
+                    //TODO: Draw 1920x1080 texture which explains how to play the game.
+                    spriteBatch.Draw(helpBackgroundTex, Game.GraphicsDevice.Viewport.Bounds, Color.White);
+                    break;
+                case MenuState.Exit:
+                    break;
+                default:
+                    break;
+            }
 
         }
     }
