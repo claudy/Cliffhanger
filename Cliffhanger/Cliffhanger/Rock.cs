dbe4b6946cf10f258a58624d610d708ed16510ab
Andrew Claudy <andrew.claudy@live.com>
2013-09-28 15:05:42 -0400

    Rock is ready to be added into level one. No animation.

--- Cliffhanger/Cliffhanger/Rock.cs
+++ Cliffhanger/Cliffhanger/Rock.cs
@@ -16,14 +16,21 @@ namespace Cliffhanger
     {
         public const float ROCKGRAVITY = -10f; // Gravity
         public Vector2 startPosition;
-        public float currentPosition;
-        public float velocity;
+        public Vector2 currentPosition;
+        public Vector2 velocity;
+        private Rectangle rect;
+        protected const int WH = 64; //Size of texture in pixel, Assumes Width == height
 
-
-        public Rock(Game game, float startXPos, float startYPos)
+        public Rock(Game game, float startXPos, float startYPos, Vector2 velocity)
             : base(game)
         {
             startPosition = new Vector2(startXPos, startYPos);
+            currentPosition = startPosition;
+            this.velocity = velocity;
+            rect = new Rectangle((int)startXPos,
+                (int)startYPos,
+                WH,
+                WH);
         }
 
         public override void Initialize()
@@ -31,14 +38,24 @@ namespace Cliffhanger
             base.Initialize();
         }
 
-        public override void Update(GameTime gameTime)
+        public void Update(GameTime gameTime, Platform ground)
         {
+            if (currentPosition.Y < ground.position.Y)
+                this.Dispose();
+
+            currentPosition.X += velocity.X;
+            currentPosition.Y += velocity.Y;
+
             base.Update(gameTime);
         }
 
-        public void Draw(SpriteBatch spriteBatch)
+        public void Draw(SpriteBatch spriteBatch, Texture2D rockTexture)
         {
-
+            rect.X = (int)currentPosition.X;
+            rect.Y = (int)currentPosition.Y;
+            rect.Width = WH;
+            rect.Height = WH;
+            spriteBatch.Draw(rockTexture, rect, Color.White);
         }
     }
 }
